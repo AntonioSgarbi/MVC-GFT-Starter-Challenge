5e3b641dd101238023597af5232297459fe4b23b
aokk <aokk@gft.com>
2022-10-30 15:28:49 -0300

    optimize imports

--- src/main/java/tech/antoniosgarbi/desafiomvc/controller/ErrorController.java
+++ src/main/java/tech/antoniosgarbi/desafiomvc/controller/ErrorController.java
@@ -2,14 +2,12 @@ package tech.antoniosgarbi.desafiomvc.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ErrorController {
